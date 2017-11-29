99087b2a30dd7f278bd365bd1497086093564c39
JermanKell <xbouchenard@gmail.com>
2017-11-29 17:23:18 +0100

    Pb Interface

--- src/View/View.java
+++ src/View/View.java
@@ -1,15 +1,9 @@
 package View;
 
 import java.util.List;
-import java.util.Observable;
 import java.util.Observer;
 
 public class View implements Observer{
 
-	@Override
-	public void update(List<Integer> stContent) {
-		// TODO Auto-generated method stub
-		
-	}
 
 }
