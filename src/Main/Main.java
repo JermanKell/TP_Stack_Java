7741912b697db3ea4dcfb1f24d3c58c63fb08a2b
arccho <xzeno@orange.fr>
2017-12-10 18:48:27 +0100

    scanner fini

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,10 +1,8 @@
 package Main;
 
-import java.util.Scanner;
-import java.util.concurrent.TimeUnit;
-
 import Stack.*;
 import View.*;
+import Menu.*;
 
 public class Main {
 
@@ -12,37 +10,6 @@ public class Main {
 		Stack pile = new Stack();
 		new View(1, pile);
 		new View(2, pile);
-		pile.Clear();
-		try {
-			TimeUnit.SECONDS.sleep(2);
-			pile.Push(5);
-			TimeUnit.SECONDS.sleep(2);
-			pile.Push(3);
-			TimeUnit.SECONDS.sleep(2);
-			pile.Push(12);
-			TimeUnit.SECONDS.sleep(2);
-			pile.Push(9);
-			TimeUnit.SECONDS.sleep(2);
-			pile.Push(63);
-			TimeUnit.SECONDS.sleep(2);
-			pile.Push(11);
-			TimeUnit.SECONDS.sleep(2);
-			pile.Push(2);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-		//Scanner sc = new Scanner(System.in);
-		
-		
-		/*while (true) {
-			System.out.println("Which action must I do ?\n");
-			String cmd = sc.nextLine();
-			if (cmd.equalsIgnoreCase("PUSH")) {
-				
-			}
-		}*/
+		new Interface(pile);
 	}
-
 }
--- src/Menu/Interface.java
+++ src/Menu/Interface.java
@@ -1,24 +1,53 @@
 package Menu;
 
 import java.util.Scanner;
+import Stack.Stack;
 
 public class Interface {
+	/** scanner qui lit*/
 	private Scanner sc;
 	
-	public Interface() {
+	/** pile*/
+	private Stack stack;
+	
+	/** Constructeur*/
+	public Interface(Stack stack) {
 		sc = new Scanner(System.in);
+		this.stack = stack;
+		run();
 	}
 	
+	/** Analyse et execute la commande saisie par l'utilisateur*/
 	private void RequestProc(String Proc) {
-		
+		if(Proc.toLowerCase().startsWith("push")) {
+			try{
+			stack.Push(Integer.parseInt(Proc.substring(5)));
+			}
+			catch(NumberFormatException e) {
+				System.out.println("Invalid command syntax. Enter push [integer]");
+			}
+		}
+		else if(Proc.toLowerCase().equals("pop")) {
+			stack.Pop();
+		}
+		else if(Proc.toLowerCase().equals("clear")) {
+			stack.Clear();
+		}
+		else if(Proc.toLowerCase().equals("quit")) {
+			System.exit(1);
+		}
+		else {
+			System.out.println("Command not found\nEnter one of these commands:\npush [integer]\npop\nclear\nquit");
+		}
 	}
 	
+	/** Terminal*/
 	public void run() {
 		String processing = null;
+		stack.Clear();
 		while (true) {
 			System.out.println("What do you like to do ?\n");
 			processing = sc.nextLine();
-			
 			RequestProc(processing);
 		}
 	}
